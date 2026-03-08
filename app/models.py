1549ac70aaa67e72c5d36c048186754b144ffc52
Imanol Ramirez <ernesto.ramirez7075@alumnos.udg.mx>
2026-03-07 23:54:06 -0600

    Agregar panel de expediente del paciente

--- app/models.py
+++ app/models.py
@@ -1,10 +1,6 @@
 from app import db, login_manager
 from flask_login import UserMixin
 from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
-db = SQLAlchemy()
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -19,64 +15,25 @@ class Usuario(UserMixin, db.Model):
 
 class Paciente(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100))
-    edad = db.Column(db.Integer)
-
-    actividad_fisica = db.relationship('ActividadFisica', backref='paciente')
-    actividad_cognitiva = db.relationship('ActividadCognitiva', backref='paciente')
-    actividad_nutricional = db.relationship('ActividadNutricional', backref='paciente')
-    actividad_medica = db.relationship('ActividadMedica', backref='paciente')
-    actividad_social = db.relationship('ActividadSocial', backref='paciente')
-    historial_clinico = db.relationship('HistorialClinico', backref='paciente')
+    nombre = db.Column(db.String(120), nullable=False)
+    edad = db.Column(db.Integer, nullable=False)
+    genero = db.Column(db.String(20), nullable=False)
+    fecha_registro = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    usuario_id = db.Column(db.Integer, db.ForeignKey("usuario.id"), nullable=False)
+    usuario = db.relationship("Usuario", backref="pacientes")
+    
+from datetime import datetime
 
 class ActividadFisica(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    tipo = db.Column(db.String(100))
-    duracion = db.Column(db.Integer)
-    fecha = db.Column(db.DateTime, default=datetime.utcnow)
+    fecha = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    tipo = db.Column(db.String(100), nullable=False)
+    duracion_min = db.Column(db.Integer, nullable=False)  # minutos
+    observaciones = db.Column(db.String(300))
 
-    paciente_id = db.Column(db.Integer, db.ForeignKey('paciente.id'))
+    paciente_id = db.Column(db.Integer, db.ForeignKey("paciente.id"), nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey("usuario.id"), nullable=False)
 
-class ActividadCognitiva(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    actividad = db.Column(db.String(100))
-    resultado = db.Column(db.String(200))
-    fecha = db.Column(db.DateTime, default=datetime.utcnow)
-
-    paciente_id = db.Column(db.Integer, db.ForeignKey('paciente.id'))
-
-class ActividadNutricional(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    comida = db.Column(db.String(200))
-    calorias = db.Column(db.Integer)
-    fecha = db.Column(db.DateTime, default=datetime.utcnow)
-
-    paciente_id = db.Column(db.Integer, db.ForeignKey('paciente.id'))
-
-class ActividadMedica(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    presion = db.Column(db.String(20))
-    glucosa = db.Column(db.String(20))
-    observaciones = db.Column(db.Text)
-    fecha = db.Column(db.DateTime, default=datetime.utcnow)
-
-    paciente_id = db.Column(db.Integer, db.ForeignKey('paciente.id'))
-
-class ActividadSocial(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    actividad = db.Column(db.String(200))
-    duracion = db.Column(db.Integer)
-    fecha = db.Column(db.DateTime, default=datetime.utcnow)
-
-    paciente_id = db.Column(db.Integer, db.ForeignKey('paciente.id'))
-
-class HistorialClinico(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    diagnostico = db.Column(db.Text)
-    tratamiento = db.Column(db.Text)
-    medicamentos = db.Column(db.Text)
-    fecha = db.Column(db.DateTime, default=datetime.utcnow)
-
-    paciente_id = db.Column(db.Integer, db.ForeignKey('paciente.id'))
-
-
+    paciente = db.relationship("Paciente", backref="actividades_fisicas")
+    usuario = db.relationship("Usuario")
